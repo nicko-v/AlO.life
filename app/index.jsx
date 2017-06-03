9182a261c00c5d87af66a28ca8b4fa24f1c00830
Nickolay Vasiliev <nicko.v@me.com>
2017-06-03 05:04:33 +0300

    Update index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,6 @@
 import React     from 'react';
 import ReactDOM  from 'react-dom';
-import App       from './components/app.jsx';
+import App       from './components/App.jsx';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 
